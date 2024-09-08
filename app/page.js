c7b52d95c6aabc1e75f7ab9e9e4c4a6e7671cc0b
skylakefreak <utkarshbarde2@gmail.com>
2024-09-08 23:44:47 +0530

    v1

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Main from "../components/Main"
+
+export default function Home() {
+  return (
+    <main className="h-screen bg-white">
+      <Main/>
+
+    </main>
+  );
+}
