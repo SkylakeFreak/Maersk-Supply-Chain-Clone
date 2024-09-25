2b3f534ad0dd460d00cf05221f7a887e9ce07110
skylakefreak <utkarshbarde2@gmail.com>
2024-09-25 23:11:31 +0530

    second page

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-function Cards() {
+function Cards({link,text1,text2,buttontext}) {
   return (
-    <div className="outline outline-1 h-auto w-full flex flex-col items-center justify-center p-5 rounded-sm">
+    <div className="outline outline-1 h-auto w-full flex flex-col items-center justify-center p-5 sm:min-h-[150px] lg:min-h-[350px] xl:min-h-[220px] rounded-sm">
       <div className="w-full h-1/3 flex items-center flex-row justify-start">
         <img
           width="50"
           height="50"
-          src="https://img.icons8.com/ios/50/test-account.png"
+          src={link}
           alt="test-account"
         />
         <div className="flex top-0 ">
-          <p className="text-xl p-2 text">New to Maersk.com?</p>
+          <p className="text-xl p-2 text">{text1}</p>
         </div>
         
       </div>
-      <div className="w-full h-1/3 flex items-center p-2 ml-5">Sign up to book, manage and pay your shipments online, and access a suite of products and services designed to simplify your supply chain.</div>
-      <div className="w-full h-1/3"><button className="bg-[#00243D] text-white p-2 rounded-sm ml-5">Register Now</button></div>
+      <div className="w-full h-1/3 flex items-center p-2 ml-5">{text2}</div>
+      <div className="w-full h-1/3"><button className="bg-[#00243D] text-white p-2 rounded-sm ml-5">{buttontext}</button></div>
     </div>
   );
 }
--- components/Cardspage.jsx
+++ components/Cardspage.jsx
@@ -4,9 +4,9 @@ import Cards from './Cards'
 function Cardspage() {
   return (
     <div className='h-auto flex flex-col p-10 md:p-20 ml-10 mr-10 lg:p-30 gap-10 lg:flex-row items-center justify-evenly'>
-        <Cards/>
-        <Cards/>
-        <Cards/>
+        <Cards link={"https://img.icons8.com/ios/50/test-account.png"} text1={"New to Maersk.com?"} text2={"Sign up to book, manage and pay your shipments online, and access a suite of products and services designed to simplify your supply chain."} buttontext={"Register Now"}/>
+        <Cards link={"https://img.icons8.com/external-flatart-icons-outline-flatarticons/50/1A1A1A/external-lifeguard-web-security-flatart-icons-outline-flatarticons.png"} text1={"How to get started"} text2={"Whether you need to find prices, place a booking, submit documents, or track your cargo, our step-by-step guides will help you find your way around."} buttontext={"Learn More"}/>
+        <Cards link={"https://img.icons8.com/ios/50/1A1A1A/guarantee--v1.png"} text1={"Become a logistics expert"} text2={"Discover insights, introductory videos, explanatory articles and gain the confidence you need to optimise your supply chain."} buttontext={"Learn More"}/>
     </div>
   )
 }
